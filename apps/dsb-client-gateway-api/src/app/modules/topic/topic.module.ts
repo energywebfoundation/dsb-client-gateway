4d41be11be8053c5c58803f0a2c3b699b6cbc7d3
Vikas Kumar <vikas.kumar@energyweb.org>
2022-05-06 13:21:18 +0530

    refactor: topics from reference cache

--- apps/dsb-client-gateway-api/src/app/modules/topic/topic.module.ts
+++ apps/dsb-client-gateway-api/src/app/modules/topic/topic.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TopicsController } from './controller/topic.controller';
-import { DsbClientGatewayStorageModule } from '@dsb-client-gateway/dsb-client-gateway-storage';
+import {
+  DsbClientGatewayStorageModule,
+  TopicRepositoryModule,
+} from '@dsb-client-gateway/dsb-client-gateway-storage';
 import { DdhubClientGatewayMessageBrokerModule } from '@dsb-client-gateway/ddhub-client-gateway-message-broker';
 import { EnrolmentModule } from '../enrolment/enrolment.module';
+import { TopicService } from './service/topic.service';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
   imports: [
+    CqrsModule,
     DsbClientGatewayStorageModule,
+    TopicRepositoryModule,
     DdhubClientGatewayMessageBrokerModule.forRootAsync([EnrolmentModule]),
   ],
   controllers: [TopicsController],
+  providers: [TopicService],
+  exports: [TopicService],
 })
 export class TopicModule {}
