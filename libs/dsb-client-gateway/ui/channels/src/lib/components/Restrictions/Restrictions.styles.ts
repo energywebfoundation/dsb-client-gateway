0e61b9352b9ed8fd2298de69d2451a18fa454c42
nadia-adila <105151876+nadia-adila@users.noreply.github.com>
2022-05-19 12:06:08 +0800

    feat(ui): Channel List Restriction DID & Role

--- libs/dsb-client-gateway/ui/channels/src/lib/components/Restrictions/Restrictions.styles.ts
+++ libs/dsb-client-gateway/ui/channels/src/lib/components/Restrictions/Restrictions.styles.ts
@@ -7,4 +7,17 @@ export const useStyles = makeStyles()((theme) => ({
     fontWeight: 400,
     color: theme.palette.grey[200],
   },
+  chip: {
+    background: theme.palette.primary.main,
+    height: 20,
+    borderRadius: 21,
+  },
+  chipLabel: {
+    fontSize: 12,
+    lineHeight: '18px',
+    fontWeight: 600,
+    letterSpacing: '0.4px',
+    color: '#fff',
+    fontFamily: theme.typography.body2.fontFamily,
+  },
 }));
--- libs/dsb-client-gateway/ui/channels/src/lib/components/Restrictions/Restrictions.tsx
+++ libs/dsb-client-gateway/ui/channels/src/lib/components/Restrictions/Restrictions.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react';
 import { Box, Typography } from '@mui/material';
 import { useStyles } from './Restrictions.styles';
+import { didFormatMinifier } from "@dsb-client-gateway/ui/utils";
+import { Collapse, Chip } from '@mui/material';
+import { useRestrictionsEffects } from './Restrictions.effects';
 
 export interface RestrictionsProps {
   value: string[];
@@ -9,11 +12,23 @@ export interface RestrictionsProps {
 
 export const Restrictions: FC<RestrictionsProps> = ({ value, type }) => {
   const { classes } = useStyles();
+  const { isOpen, handleOpening } = useRestrictionsEffects();
+  let formattedValue;
+
+  if (value?.length) {
+    formattedValue = (type === 'DID') ? value.map(item => didFormatMinifier(item)) : value;
+  }
+
   return (
     <Box>
-      <Typography variant="body2" className={classes.text}>
-        {(value?.length === 0 || !value) ? '--' : type}
-      </Typography>
+      {(!isOpen ?
+        (<Typography variant="body2" className={classes.text} sx={{ display: 'inline', marginRight: '8px' }}>
+        { value?.length ? formattedValue.slice(0,3).join(', ') : '--' }</Typography>)
+        : (<Collapse in={isOpen} timeout="auto" unmountOnExit>
+            <Typography variant="body2" className={classes.text}>{formattedValue.join(', ')}</Typography></Collapse>)
+      )}
+
+      {(!isOpen && value?.length > 3) ? <Chip label={`+${value.length - 3}`} onClick={handleOpening} className={classes.chip} classes={{label: classes.chipLabel}}/> : ''}
     </Box>
   );
 };
